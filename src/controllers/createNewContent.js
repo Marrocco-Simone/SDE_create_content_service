adcbe173d27efef1b3bd4b8f53d0fd365c185cec
Marrocco-Simone <marrocco.simone00@gmail.com>
2023-02-25 23:04:16 +0100

    blogger api added

--- src/controllers/createNewContent.js
+++ src/controllers/createNewContent.js
@@ -3,6 +3,29 @@ const { default: fetch } = require("node-fetch");
 
 const data_service_url = process.env.DATA_SERVICE_URL;
 const dalle_service_url = process.env.DALLE_SERVICE_URL;
+const blogger_service_url = process.env.BLOGGER_SERVICE_URL;
+
+async function makeAFetch(url, method, token, body, item_description) {
+  console.log(`starting generation of ${item_description}`);
+
+  const result = await fetch(url, {
+    method,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(body),
+  });
+
+  const result_json = await result.json();
+
+  if (result.status !== 200) {
+    console.log(result_json);
+    throw new Error(`could not generate ${item_description}`);
+  } else console.log(`${item_description} correctly generated`);
+
+  return result_json;
+}
 
 async function createNewContent(req, res) {
   try {
@@ -11,47 +34,50 @@ async function createNewContent(req, res) {
     }
 
     const { prompt } = req.body;
+    const token = req.token;
 
     res.send({ received: true });
 
-    const dalle_result = await fetch(`${dalle_service_url}/image`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ prompt }),
-    });
+    // * blogger api
+    const blogger_result_json = await makeAFetch(
+      `${blogger_service_url}/generateText`,
+      "POST",
+      token,
+      { prompt },
+      "content text"
+    );
+    const content_text = blogger_result_json.text;
 
-    if (dalle_result.status === 200) {
-      console.log("Images correctly generated");
-    } else console.error("Could not generate new images");
-
-    const dalle_result_json = await dalle_result.json();
+    // * dalle api
+    const dalle_result_json = await makeAFetch(
+      `${dalle_service_url}/image`,
+      "POST",
+      token,
+      { prompt },
+      "images"
+    );
     const img_url = dalle_result_json.url;
-
     const img_result = await fetch(img_url);
     const img_buffer = await img_result.buffer();
     const img_b64 = img_buffer.toString("base64");
 
+    // * store everything api
+    const title = `${prompt}`;
     const body = {
-      title: `${prompt}`,
-      // TODO this should be obtained by the chatgpt api
-      content_text: "my content text",
+      title,
+      content_text,
       img_b64,
     };
 
-    const database_result = await fetch(`${data_service_url}/db/store`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${req.token}`,
-      },
-      body: JSON.stringify(body),
-    });
+    const database_result_json = await makeAFetch(
+      `${data_service_url}/db/store`,
+      "PUT",
+      token,
+      body,
+      "record in database"
+    );
 
-    if (database_result.status === 200) {
-      console.log("Insert correctly done");
-    } else console.error("Could not insert new content");
+    console.log(`Generade new content with id: ${database_result_json._id}`);
   } catch (e) {
     console.error(e);
   }
